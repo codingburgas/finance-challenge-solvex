2bdf7ea596c9a849619d8e1a300507dbe71c6b5a
Stefan Radev <SDRadev22@codingburgas.bg>
2024-10-27 20:59:07 +0200

    Initial code commit

--- Solvex/Solvex.cpp
+++ Solvex/Solvex.cpp
@@ -1,20 +1,95 @@
-// Solvex.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#define RAYGUI_IMPLEMENTATION
+#define _CRT_SECURE_NO_WARNINGS
+#include "raylib.h"
+#include "raygui.h"
+#include<string>
+#include<iostream>
 
-#include <iostream>
+enum WindowState {
+    LOGIN,
+    SIGNUP,
+    APP
+};
 
-int main()
+// Program main entry point
+
+int main(void)
 {
-    std::cout << "Hello World!\n";
+    // Initialization
+
+    const int screenWidth = 1000;
+    const int screenHeight = 500;
+
+    //ALL BOXES WILL HAVE THEIR BOUNDS SET HERE BEFORE INITIALISATION UNLESS THEY'RE DYNAMIC (i didnt bother doing this for half of them) 
+    const Rectangle UsernameInputBounds = { screenWidth / 2 - 60, screenHeight / 2 - 100, 120, 24 };
+    const Rectangle UsernamePasswordBounds = { screenWidth / 2 - 60, screenHeight / 2 - 50, 120, 24 };
+
+    //will not keep the name giggidy for the window state i just couldn't think of a word yet (i am 90% sure this will not be changed)
+    WindowState giggidy = LOGIN;
+    Vector2 MousePosition;
+    bool LoginButtonPressed = false;
+    char UsernameinputText[32] = "Username";
+    char PasswordinputText[32] = "Password";
+
+    InitWindow(screenWidth, screenHeight, "Solvex");
+
+    SetTargetFPS(60);
+
+    // Main game loop
+    while (!WindowShouldClose())
+    {
+        // Update your variables here BEFORE DRAWING unless necessary
+        MousePosition = GetMousePosition();
+        if (LoginButtonPressed)
+        {
+            LoginButtonPressed = false;
+            giggidy = APP;
+            std::cout << UsernameinputText << std::endl;
+        }
+
+
+        // Draw
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+
+        switch (giggidy)
+        {
+        case LOGIN:
+            GuiTextBox(UsernameInputBounds, UsernameinputText, 32, CheckCollisionPointRec(MousePosition, UsernameInputBounds));
+            GuiTextBox(UsernamePasswordBounds, PasswordinputText, 32, CheckCollisionPointRec(MousePosition, UsernamePasswordBounds));
+            LoginButtonPressed = GuiButton({ screenWidth/2 - 60, screenHeight/2, 120, 24 }, "Login");
+            break;
+        case SIGNUP:
+
+            break;
+        case APP:
+
+            //THIS IS ONLY FOR TESTING PURPOSES
+            for (int i = 0; i < 5; i++)
+            {
+                if (i % 2 == 0) DrawRectangle(screenWidth / 2, (30 + 20 * i), 200, 20, Fade(LIGHTGRAY, 0.5f));
+                else DrawRectangle(screenWidth / 2, (30 + 20 * i), 200, 20, Fade(LIGHTGRAY, 0.3f));
+
+                if (i < 5 - 1)
+                {
+                    DrawText("word", screenWidth / 2, (36 + 20 * i), 10, DARKGRAY);
+                    DrawText("num", screenWidth / 2 + 30, (36 + 20 * i), 10, DARKGRAY);
+                }
+                else
+                {
+                    DrawText("rizz", screenWidth / 2, (36 + 20 * i), 10, MAROON);
+                    DrawText("13", screenWidth / 2 + 30, (36 + 20 * i), 10, MAROON);
+                }
+            }
+            break;
+        default: break;
+        }
+
+        EndDrawing();
+    }
+    // De-Initialization
+    CloseWindow();// Close window and OpenGL context
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
