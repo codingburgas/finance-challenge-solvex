55717040bfdb8cf91ab1e9f8d35f940f7b188b18
Stefan Radev <SDRadev22@codingburgas.bg>
2024-11-01 18:16:49 +0200

    Rename Variables Add tab selection

--- Solvex/src/Solvex.cpp
+++ Solvex/src/Solvex.cpp
@@ -27,22 +27,27 @@ int main(void)
 {
     // Initialization
 
-    const int screenWidth = 1000;
-    const int screenHeight = 500;
+    const int screenWidth = 1280;
+    const int screenHeight = 720;
 
-    //ALL BOXES WILL HAVE THEIR BOUNDS SET HERE BEFORE INITIALISATION UNLESS THEY'RE DYNAMIC (i didnt bother doing this for half of them) 
-    const Rectangle UsernameInputBounds = { screenWidth / 2 - 60, screenHeight / 2 - 100, 120, 24 }; // 
-    const Rectangle UsernamePasswordBounds = { screenWidth / 2 - 60, screenHeight / 2 - 50, 120, 24 };
+    //ALL BOXES WILL HAVE THEIR BOUNDS SET HERE BEFORE INITIALISATION (i didnt bother doing this for half of them) 
+    const Rectangle usernameInputBounds = { screenWidth / 2 - 60, screenHeight / 2 - 100, 120, 24 };
+    const Rectangle usernamePasswordBounds = { screenWidth / 2 - 60, screenHeight / 2 - 50, 120, 24 };
+    const Rectangle saveButtonBounds = { 0, 0, 60, 24 };
+    const Rectangle tabButtonBounds = { 30, 100, 60, 24 };
 
     //will not keep the name giggidy for the window state i just couldn't think of a word yet (i am 90% sure this will not be changed)
-    WindowState giggidy = ACCESS;
-    Vector2 MousePosition;
-    bool LoginButtonPressed = false;
-    bool SignupButtonPressed = false;
-    char UsernameinputText[32] = "Username";
-    char PasswordinputText[32] = "Password";
+    WindowState Window = ACCESS;
+    Vector2 mousePosition;
+    bool loginButtonPressed = false;
+    bool signupButtonPressed = false;
+    bool saveButtonPressed = false;
+    char usernameInputText[32] = "Username";
+    char passwordInputText[32] = "Password";
+    char newTabInputText[32] = "";
 
     std::vector<TAB_STRUCT> account;
+    int selectedTab = 0;
 
     InitWindow(screenWidth, screenHeight, "Solvex");
 
@@ -52,22 +57,22 @@ int main(void)
     while (!WindowShouldClose())
     {
         // Update your variables here BEFORE DRAWING unless necessary
-        MousePosition = GetMousePosition();
+        mousePosition = GetMousePosition();
         //Window state specific logic goes here (so it doesnt run needlessly (beep boop totally made by chatgpt))
-        switch (giggidy)
+        switch (Window)
         {
         case ACCESS:
-            if (LoginButtonPressed)
+            if (loginButtonPressed)
             {
-                LoginButtonPressed = false;
+                loginButtonPressed = false;
 
-                giggidy = APP;
-                std::cout << UsernameinputText << std::endl;
+                Window = APP;
+                std::cout << usernameInputText << std::endl;
             }
-            if (SignupButtonPressed)
+            if (signupButtonPressed)
             {
                 account.push_back({
-                    UsernameinputText, // tabName for the new subaccount
+                    usernameInputText, // tabName for the new subaccount
                     {
                       {
                         "Transaction 1",
@@ -82,47 +87,58 @@ int main(void)
                 account[0].transaction.push_back({ "Transaction 3", 300.0f });
             }
             break;
+
         case APP:
-        {
+            if (saveButtonPressed)
+            {
+                saveButtonPressed = false;
+
+                std::cout << "no saving implemented yet lolololol" << std::endl;
+            }
             break;
         }
-        }
-
-
         // Draw
         BeginDrawing();
 
         ClearBackground(RAYWHITE);
-
-        switch (giggidy)
+        
+        switch (Window)
         {
         case ACCESS:
-            GuiTextBox(UsernameInputBounds, UsernameinputText, 32, CheckCollisionPointRec(MousePosition, UsernameInputBounds));
-            GuiTextBox(UsernamePasswordBounds, PasswordinputText, 32, CheckCollisionPointRec(MousePosition, UsernamePasswordBounds));
 
-            LoginButtonPressed = GuiButton({ screenWidth/2 - 60, screenHeight/2, 60, 24 }, "Login");
-            SignupButtonPressed = GuiButton({ screenWidth/2, screenHeight/2, 60, 24 }, "Signup");
+            GuiTextBox(usernameInputBounds, usernameInputText, 32, CheckCollisionPointRec(mousePosition, usernameInputBounds));
+            GuiTextBox(usernamePasswordBounds, passwordInputText, 32, CheckCollisionPointRec(mousePosition, usernamePasswordBounds));
+
+            loginButtonPressed = GuiButton({ screenWidth/2 - 60, screenHeight/2, 60, 24 }, "Login");
+            signupButtonPressed = GuiButton({ screenWidth/2, screenHeight/2, 60, 24 }, "Signup");
             break;
+
         case APP:
-
-            //THIS IS ONLY FOR TESTING PURPOSES
-            for (int i = 0; i < account[0].transaction.size(); i++)
+            saveButtonPressed = GuiButton(saveButtonBounds, "Save");
+            for (size_t i = 0; i <= account.size(); i++)
             {
-                if (i % 2 == 0) DrawRectangle(screenWidth / 2, (30 + 20 * i), 200, 20, Fade(LIGHTGRAY, 0.5f));
-                else DrawRectangle(screenWidth / 2, (30 + 20 * i), 200, 20, Fade(LIGHTGRAY, 0.3f));
-
-                if (i < account[0].transaction.size() - 1)
+                Rectangle iTabButtonBounds = tabButtonBounds;
+                iTabButtonBounds.x += tabButtonBounds.width*i;
+                if (i < account.size())
                 {
-                    DrawText(account[0].transaction[i].note.c_str(), screenWidth / 2, (36 + 20 * i), 10, DARKGRAY);
-                    DrawText("12", screenWidth / 2 + 80, (36 + 20 * i), 10, DARKGRAY);
+                    if (GuiButton(iTabButtonBounds, account[i].tabName.c_str())) {
+                        selectedTab = i;
+                    }
                 }
                 else
                 {
-                    DrawText(account[0].transaction[i].note.c_str(), screenWidth / 2, (36 + 20 * i), 10, MAROON);
-                    DrawText("13", screenWidth / 2 + 80, (36 + 20 * i), 10, MAROON);
+                    if (GuiButton(iTabButtonBounds, "Add")) {
+                        account.push_back({newTabInputText});
+                        newTabInputText[0] = '\0';
+                    }
+
+                    Rectangle newTabInputBounds = iTabButtonBounds;
+                    newTabInputBounds.y += tabButtonBounds.height;
+                    GuiTextBox(newTabInputBounds, newTabInputText, 32, CheckCollisionPointRec(mousePosition, newTabInputBounds));
                 }
             }
             break;
+
         }
 
         EndDrawing();
